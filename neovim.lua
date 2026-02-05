7cee1b0b59139432ec3f02a861a3348d885bbe4c
HANCORE <230438592+HANCORE-linux@users.noreply.github.com>
2026-02-05 19:32:42 +0100

    Add files via upload

--- neovim.lua
+++ neovim.lua
@@ -1,16 +1,9 @@
 return {
+  { "rose-pine/neovim", name = "rose-pine" },
   {
-    "EdenEast/nightfox.nvim",
-    priority = 1000, -- load before other plugins
-    config = function()
-      require("nightfox").setup({
-        options = {
-          transparent = false, -- set true if you want transparent background
-        },
-      })
-
-      vim.cmd("colorscheme dayfox")
-	  vim.g.lightline = { colorscheme = "dayfox" }
-    end,
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine-dawn",
+    },
   },
 }
